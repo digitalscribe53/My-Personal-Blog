b9526c8eb880eb22de95e450aa4fac5b07d18e4e
Kent Ball <kent.vincent.ball@gmail.com>
2024-06-01 11:42:37 -0400

    Added Dark Mode

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function darkMode () {
+   
+   let leftBox = document.getElementById("left-box");
+   let rightBox = document.getElementById("right-box");
+   leftBox.classList.toggle("dark-mode");
+   rightBox.classList.toggle("dark-mode");
+
+}
