09bf9bbcfc9ed141d5fe97139136d9dedc494e1f
Kent Ball <kent.vincent.ball@gmail.com>
2024-06-01 16:50:08 -0400

    Added onclick redirect to Blog page

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,21 @@ function darkMode () {
    
    let leftBox = document.getElementById("left-box");
    let rightBox = document.getElementById("right-box");
+   let blogContainer = document.getElementById("blog-container");
    leftBox.classList.toggle("dark-mode");
    rightBox.classList.toggle("dark-mode");
+   blogContainer.classList.toggle("dark-mode");
+}
 
+let blogPosts = [];
+
+
+
+function saveText() {
+   /* Load the Blog Posts page when submit btn is clicked */
+   window.location = "./blog.html";
+   
+   let textArea = document.getElementById("blog-text").value;
+   
+   document.getElementById("first-article").innerText = textArea;
 }
