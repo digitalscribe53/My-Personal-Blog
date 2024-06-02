09aaa01bef90dee3b213e6929bcd81cfb858c41a
Kent Ball <kent.vincent.ball@gmail.com>
2024-06-01 23:54:03 -0400

    Added Back button to Blog page

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,9 @@ function darkMode () {
    
    let leftBox = document.getElementById("left-box");
    let rightBox = document.getElementById("right-box");
-   let blogContainer = document.getElementById("blog-container");
    leftBox.classList.toggle("dark-mode");
    rightBox.classList.toggle("dark-mode");
-   blogContainer.classList.toggle("dark-mode");
+   
 }
 
 let blogPosts = [];
@@ -15,8 +14,13 @@ let blogPosts = [];
 function saveText() {
    /* Load the Blog Posts page when submit btn is clicked */
    window.location = "./blog.html";
-   
+   let blogContainer = document.getElementById("blog-container");
+   blogContainer.classList.toggle("dark-mode");
    let textArea = document.getElementById("blog-text").value;
    
    document.getElementById("first-article").innerText = textArea;
+}
+
+function goBack() {
+   window.location = "./index.html";
 }
