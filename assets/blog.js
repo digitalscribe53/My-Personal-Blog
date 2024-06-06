802681dad1600cadb04eff4e62255f6222359bad
Kent Ball <kent.vincent.ball@gmail.com>
2024-06-06 19:27:03 -0400

    Fixed issue with eventListener in logic.js

--- assets/blog.js
+++ assets/blog.js
@@ -1,44 +1,33 @@
 
 
 
-
-
-
 function loadBlog() {
    
-    //window.location = "./blog.html";
     const firstArticle = document.getElementById("first-article");
     let storedPostsArrayJSON = localStorage.getItem("postsArray");
     let storedPostsArray = JSON.parse(storedPostsArrayJSON);
 
     console.log(storedPostsArray);
 
+    // Iterate over the storedPostsArray
     storedPostsArray.forEach(post => {
+      // Create a div for each post
       const postDiv = document.createElement('div');
-
+      // Create elements for username, title and content
       const usernameEl = document.createElement('h3');
       const titleEl = document.createElement('h3');
       const contentEl = document.createElement('p');
-
+      // Set the innerText for each element
       usernameEl.innerText = `By: ${post.username}`;
       titleEl.innerText = post.title;
       contentEl.innerText = post.content;
-
+      // Append the elements to the postDiv
       postDiv.appendChild(usernameEl);
       postDiv.appendChild(titleEl);
       postDiv.appendChild(contentEl);
-
+      // Append the postDiv to the container
       firstArticle.appendChild(postDiv);
     })
-    
-    
-    //firstArticle.innerText = storedPostsArray.title + "\nby: " + storedPostsArray.username + "\n" + storedPostsArray.content;
-
-
-    
-    
-    
-
 }   
 
   loadBlog();
@@ -51,7 +40,5 @@ function loadBlog() {
 
  function goBack() {
     window.location = "./index.html"
-    //if (localStorage.getItem('dark-mode')) {
-      // body.classList.add('dark-mode');
-    //}
+    
  }
--- assets/logic.js
+++ assets/logic.js
@@ -16,97 +16,37 @@
 
 if (submitForm) {
    
-
-
 // change event
-submitForm.addEventListener('click', function (event) {
+submitForm.addEventListener('submit', function (event) {
    event.preventDefault();
-   //let btnClick = event.currentTarget;
+   
 
-
-   if (event.target = submitBtn) {
+   
+   //if (event.target = submitBtn) {
    if (userName.value === '' || title.value === '' || blogText.value === '') {
       
       alert("Please fill in all the fields.");
    } else { 
-      /*const user = {
-         username: userName.value,
-         title: title.value,
-         content: blogText.value
-      }
-      */
+      
 
       let retrievedArray = localStorage.getItem('postsArray');
       let parsedRetrievedArray = JSON.parse(retrievedArray);
       
       postsArray = parsedRetrievedArray;
       
-      
-
-
       postsArray.push({username: userName.value, title: title.value, content: blogText.value});
       console.log(postsArray);
 
       let stringifiedPostsArray = JSON.stringify(postsArray);
       localStorage.setItem('postsArray', stringifiedPostsArray);
 
-      //localStorage.getItem("postsArray", JSON.parse(postsArray));
-      
-      
-
-      // localStorage.getItem(postsArray) EACH TIME MAIN LOADS
-      // THEN LISTEN FOR CLICK AND PUSH VALUES TO ARRAY AGAIN 
-
-      
-
-      //localStorage.setItem("user", JSON.stringify(user));
-
-      /* for each time the submit btn is clicked, push the current iteration of values to the array in localStorage.*/
-      //for (let i = 0; i < postsArray.length; i++) {
-         
-      //}
-
-
-
-      //const stringifiedUser = JSON.stringify(user);
       
       window.location = "./blog.html";
-      
-      //console.log(userParsed);
-      
-
-      
-      
    }  
-}
+//}
 });
 }
 
-/*
-document.getElementById("submit-btn").onclick = function() {
-   /*
-   const allFilled = true;
-   const userName = document.getElementById("username");
-   const title = document.getElementById("title");
-   const blogText = document.getElementById("blog-text");
-   
-   if (userName.value === null || title.value === null || blogText.value === null) 
-      
-      alert("Please fill in all the fields.");
-   
-} */
-   /*
-   const message = ("Please enter a Username, Title and Content before clicking Submit.");
-   const msgBox = document.getElementById("warning-msg");
-   const userName = document.getElementById("username");
-   const title = document.getElementById("title");
-   const blogText = document.getElementById("blog-text");
-   
-   if (userName.value === null || title.value === null || blogText.value === null) {
-      
-      document.msgBox.innerHTML = message;
-   } 
-} */
 
    
 function darkMode() {
@@ -119,21 +59,8 @@ function darkMode() {
 
 
 
- // Load the Blog Posts page when submit btn is clicked 
+ 
 
-/*   
-   window.onload = function() {   
-      let userName = localStorage.getItem('username');
-      if (userName != "undefined" || userName != "null") {
-         document.getElementById('first-article').innerHTML = userName;
-      } else {
-         document.getElementById('first-article').innerHTML = "Hello there!";
-      }
-   } 
-   */
-
-/*
-// save form values to local storage   
 window.onload = function() {
 
    
@@ -143,21 +70,5 @@ window.onload = function() {
       
    });
   
-} */
+} 
 
-// Go back to main page when 'back' button is clicked 
-
-
-/*
-let checkbox = document.getElementById("dark-mode");
-let body = document.getElementById('body');
-
-checkbox.addEventListener( 'change', function() {
-   localStorage.setItem('dark-mode',this.checked);
-   if (this.checked) {
-      body.classList.add('dark-mode')
-   } else {
-      body.classList.remove('dark-mode')
-   }
-   }); 
-   */ 
